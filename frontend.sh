c8db1176ff2c2fa55008351e26c89a65f92c2386
priyanshuprafful <priyanshuprafful@gmail.com>
2023-09-01 16:20:47 +0530

    updating ./roboshop_shell

--- frontend.sh
+++ frontend.sh
@@ -3,7 +3,7 @@ log_file=/tmp/roboshop.log # automatically the location and file will created he
 rm -f ${log_file}
 
 print_head() {
-  echo -e "\e[35m${input}\e[0m"
+  echo -e "\e[35m${1}\e[0m"
 }
 
 print_head "Installing Nginx"
@@ -21,10 +21,10 @@ unzip /tmp/frontend.zip &>>${log_file}
 print_head "Copying Nginx Configurations for Roboshop"
 cp ${code_dir}/configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${log_file}
 
-print_head Enabling Nginx\e[0m"
+print_head "Enabling Nginx"
 systemctl enable nginx &>>${log_file}
 
-print_head Starting Nginx\e[0m"
+print_head "Starting Nginx"
 systemctl restart nginx &>>${log_file} # because we might modify the configurations
 
 # Roboshop Configs is not copied # now it is copied # now we have copied and solved our issue
