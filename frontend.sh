5d11a8762e5ae0a4ee11f16c4c20ce033b947722
priyanshuprafful <priyanshuprafful@gmail.com>
2023-09-01 16:19:05 +0530

    updating ./roboshop_shell

--- frontend.sh
+++ frontend.sh
@@ -2,25 +2,29 @@ code_dir=$(pwd)
 log_file=/tmp/roboshop.log # automatically the location and file will created here
 rm -f ${log_file}
 
-echo -e "\e[35mInstalling Nginx\e[0m"
+print_head() {
+  echo -e "\e[35m${input}\e[0m"
+}
+
+print_head "Installing Nginx"
 yum install nginx -y &>>${log_file}
 
-echo -e "\e[35mRemoving Old Content\e[0m"
+print_head "Removing Old Content"
 rm -rf /usr/share/nginx/html/* &>>${log_file}
 
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${log_file}
 
-echo -e "\e[35mExtracting Downloaded Content\e[0m"
+print_head "Extracting Downloaded Content"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip &>>${log_file}
 
-echo -e "\e[35mCopying Nginx Configurations for Roboshop\e[0m"
+print_head "Copying Nginx Configurations for Roboshop"
 cp ${code_dir}/configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${log_file}
 
-echo -e "\e[35mEnabling Nginx\e[0m"
+print_head Enabling Nginx\e[0m"
 systemctl enable nginx &>>${log_file}
 
-echo -e "\e[35mStarting Nginx\e[0m"
+print_head Starting Nginx\e[0m"
 systemctl restart nginx &>>${log_file} # because we might modify the configurations
 
 # Roboshop Configs is not copied # now it is copied # now we have copied and solved our issue
