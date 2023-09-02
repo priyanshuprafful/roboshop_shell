b2328b75cb5f212a79114632b2ee7a1c0afcfc70
priyanshuprafful <priyanshuprafful@gmail.com>
2023-09-02 23:50:12 +0530

    updating ./roboshop_shell

--- mysql.sh
+++ mysql.sh
@@ -1,28 +1,28 @@
 source common.sh
 mysql_root_password=$1
-if [ -z "${my_sql_password}" ]; then
+if [ -z "${mysql_root_password}" ]; then
   echo -e "\e[31mMissing Mysql root password argument\e[0m"
   exit 1
 fi
 
 
 print_head "Disabling MySql 8 version"
-yum module disable mysql -y
+yum module disable mysql -y &>>${log_file}
 status_check $?
 
-cp ${code_dir}/configs/mysql.repo /etc/yum.repos.d/mysql.repo
+cp ${code_dir}/configs/mysql.repo /etc/yum.repos.d/mysql.repo &>>${log_file}
 
 print_head "Installing MySql server"
-yum install mysql-community-server -y
+yum install mysql-community-server -y &>>${log_file}
 status_check $?
 
 print_head "Enable Mysql service"
-systemctl enable mysqld
+systemctl enable mysqld &>>${log_file}
 status_check $?
 
 print_head "Start Mysql Service"
-systemctl start mysqld
+systemctl restart mysqld &>>${log_file}
 status_check $?
 
 print_head "Set Password"
-mysql_secure_installation --set-root-pass ${mysql_root_password}
+mysql_secure_installation --set-root-pass ${mysql_root_password} &>>${log_file}
