0474b5ca0332fe58a6a69c5894a5cb66cf7cd7a5
priyanshuprafful <priyanshuprafful@gmail.com>
2023-09-02 15:36:11 +0530

    updating ./roboshop_shell

--- catalogue.sh
+++ catalogue.sh
@@ -51,7 +51,7 @@ systemctl restart catalogue &>>${log_file} # restart so that any change it will 
 status_check $?
 
 print_head "Copy MongoDB repo files"
-cp configs mongodb.repo /etc/yum.repos.d/mongo.repo &>>${log_file}
+cp ${code_dir}/configs mongodb.repo /etc/yum.repos.d/mongo.repo &>>${log_file}
 status_check $?
 
 print_head "Install Mongo Client"
