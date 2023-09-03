cf353e7334bddb715f2e0021fc9b1ff5a81392ee
priyanshuprafful <priyanshuprafful@gmail.com>
2023-09-03 12:56:18 +0530

    updating ./roboshop_shell

--- common.sh
+++ common.sh
@@ -16,6 +16,24 @@ status_check() {
   fi
 }
 
+systemd_setup() {
+    print_head "Copy SystemD service files"
+    cp ${code_dir}/configs/${component}.service /etc/systemd/system/${component}.service &>>${log_file}
+    status_check $?
+
+    print_head "Reload SystemD"
+    systemctl daemon-reload &>>${log_file}
+    status_check $?
+
+    print_head "Enable ${component} service"
+    systemctl enable ${component} &>>${log_file}
+    status_check $?
+
+    print_head "Start ${component} Service"
+    systemctl restart ${component} &>>${log_file} # restart so that any change it will get impacted
+    status_check $?
+}
+
 schema_setup() {
   if [ "${schema_type}" == "mongo" ]; then
 
@@ -30,7 +48,44 @@ schema_setup() {
     print_head "Load Schema"
     mongo --host mongodb.saraldevops.online </app/schema/${component}.js &>>${log_file}
     status_check $?
+  elif [ "${schema_type}" == "mysql" ]; then
+    print_head "Install Mysql Client"
+    yum install mysql -y &>>${log_file}
+    status_check $?
+
+    print_head "Load Schema"
+    mysql -h mysql.saraldevops.online -uroot -p${mysql_root_password} < /app/schema/shipping.sql &>>${log_file}
   fi
+}
+
+app_prereq_setup() {
+  print_head "Create Roboshop User"
+    id roboshop &>>${log_file}
+    if [ $? -ne 0 ]; then
+      useradd roboshop &>>${log_file}
+    fi
+    status_check $?
+
+    print_head "Create Application Directory"
+    if [ ! -d /app ]; then # here ! will inverse the logic also mkdir has options for all this well
+      mkdir /app &>>${log_file}
+    fi
+    status_check $?
+
+    print_head "Delete Old Content"
+    rm -rf /app/* &>>${log_file} # because re run of code should not fail , hence deleting the files
+    status_check $?
+
+    print_head "Downloading App Content"
+    curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log_file}
+    status_check $?
+
+    cd /app
+
+    print_head "Extracting App Content"
+    unzip /tmp/${component}.zip &>>${log_file}
+    status_check $?
+
 }
 
 nodejs() {
@@ -38,57 +93,41 @@ nodejs() {
   curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log_file}
   status_check $?
 
+  app_prereq_setup
+
   print_head "Install NodeJS"
   yum install nodejs -y &>>${log_file}
   status_check $?
 
-  print_head "Create Roboshop User"
-  id roboshop &>>${log_file}
-  if [ $? -ne 0 ]; then
-    useradd roboshop &>>${log_file}
-  fi
-  status_check $?
 
-  print_head "Create Application Directory"
-  if [ ! -d /app ]; then # here ! will inverse the logic also mkdir has options for all this well
-    mkdir /app &>>${log_file}
-  fi
-  status_check $?
-
-  print_head "Delete Old Content"
-  rm -rf /app/* &>>${log_file} # because re run of code should not fail , hence deleting the files
-  status_check $?
-
-  print_head "Downloading App Content"
-  curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log_file}
-  status_check $?
-
-  cd /app
-
-  print_head "Extracting App Content"
-  unzip /tmp/${component}.zip &>>${log_file}
-  status_check $?
 
   print_head "Installing NodeJS dependencies"
   npm install &>>${log_file}
   status_check $?
 
-  print_head "Copy SystemD service files"
-  cp ${code_dir}/configs/${component}.service /etc/systemd/system/${component}.service &>>${log_file}
-  status_check $?
-
-  print_head "Reload SystemD"
-  systemctl daemon-reload &>>${log_file}
-  status_check $?
-
-  print_head "Enable ${component} service"
-  systemctl enable ${component} &>>${log_file}
-  status_check $?
-
-  print_head "Start ${component} Service"
-  systemctl restart ${component} &>>${log_file} # restart so that any change it will get impacted
-  status_check $?
-
   schema_setup
+
+  systemd_setup
+
+}
+
+java() {
+  print_head "Install Maven"
+  yum install maven -y &>>${log_file}
+  status_check $?
+
+  app_prereq_setup
+
+  print_head "Downloading Dependencies and Package"
+  mvn clean package &>>${log_file}
+  mv target/${component}-1.0.jar ${component}.jar &>>${log_file}
+  status_check $?
+
+  #Schema Setup Function
+  schema_setup
+
+  # SystemD Function
+  systemd_setup
+
 
 }
